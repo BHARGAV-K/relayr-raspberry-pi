68d7a68c0fa0ba97e061b7a47b98196488f1f20b
dominicwhyte <dwhyte@princeton.edu>
2016-06-14 16:30:15 +0200

    Update publish_data_mqtt.py

--- publish_data_mqtt.py
+++ publish_data_mqtt.py
@@ -82,19 +82,18 @@ def main(credentials, publishing_period):
     if publishing_period < 200:
         publishing_period = 200
 
-while True:
-    client.loop()
+    while True:
+        client.loop()
         # read sensor
-        device_id = '<my_device_id>'
+        device_id = '28-0000075dbdd5'
         sensor_value = read_temperature(device_id)
         # publish temerature data
         message = {
             'meaning': 'temperature',
             'value': sensor_value
-    }
+        }
         client.publish(credentials['topic'] + 'data', json.dumps(message))
         time.sleep(publishing_period / 1000.)
-
 
 if __name__ == '__main__':
     main(creds, publishing_period)
