52d8f1389f9f22aa06104e522f05c6a73f974814
dominicwhyte <dwhyte@princeton.edu>
2016-06-14 16:31:36 +0200

    Update publish_temperature.py

--- publish_temperature.py
+++ publish_temperature.py
@@ -1,12 +1,12 @@
-while True:
-    client.loop()
+    while True:
+        client.loop()
         # read sensor
-        device_id = '<my_device_id>'
+        device_id = '28-0000075dbdd5'
         sensor_value = read_temperature(device_id)
         # publish temerature data
         message = {
             'meaning': 'temperature',
             'value': sensor_value
-    }
+        }
         client.publish(credentials['topic'] + 'data', json.dumps(message))
         time.sleep(publishing_period / 1000.)
